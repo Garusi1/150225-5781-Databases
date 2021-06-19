2526a518ff56310d870f7cdb28f6cb780d042146
Garusi1 <44749535+Garusi1@users.noreply.github.com>
2021-06-20 00:04:57 +0300

    Delete ‏‏quar(5).sql

--- ‏‏quar(5).sql
+++
@@ -1,5 +0,0 @@
-
-SELECT *  FROM (select * from FIREARMS where SPid in (select spid from product_subcategory where product_subcategory.supplier= 'us' or product_subcategory.supplier = 'US'))
-union
-SELECT *  FROM (select * from heavy_weapones where SPid in (select spid from product_subcategory where product_subcategory.supplier= 'us' or product_subcategory.supplier = 'US'))
-
