d212c662ccca9d7540b5bdd8cf53b4502c3c2331
Garusi1 <44749535+Garusi1@users.noreply.github.com>
2021-06-20 00:06:33 +0300

    Delete ‏‏‏‏quar(6).sql

--- ‏‏‏‏quar(6).sql
+++
@@ -1,8 +0,0 @@
-
-select *
-from heavy_weapones
-where SPID in (SELECT SPID
-              FROM PRODUCT_SUBCATEGORY
-              WHERE (TYPE like '%tank%'
-              and
-              supplier = 'us' or supplier='US'))
